a272d471c439c0010e97e6787568a6c92abe1947
Darkcodelab <deepachakaravarthi434@gmail.com>
2021-07-04 19:58:56 +0530

    File Upload

--- quiz-main/quiz-main/js/questions.js
+++ quiz-main/quiz-main/js/questions.js
@@ -1,6 +1,6 @@
 // creating an array and passing the number, questions, options, and answers
 let questions = [
-    {
+  {
     numb: 1,
     question: "What does HTML stand for?",
     answer: "Hyper Text Markup Language",
@@ -8,10 +8,10 @@ let questions = [
       "Hyper Text Preprocessor",
       "Hyper Text Markup Language",
       "Hyper Text Multiple Language",
-      "Hyper Tool Multi Language"
-    ]
+      "Hyper Tool Multi Language",
+    ],
   },
-    {
+  {
     numb: 2,
     question: "What does CSS stand for?",
     answer: "Cascading Style Sheet",
@@ -19,10 +19,10 @@ let questions = [
       "Common Style Sheet",
       "Colorful Style Sheet",
       "Computer Style Sheet",
-      "Cascading Style Sheet"
-    ]
+      "Cascading Style Sheet",
+    ],
   },
-    {
+  {
     numb: 3,
     question: "What does PHP stand for?",
     answer: "Hypertext Preprocessor",
@@ -30,10 +30,10 @@ let questions = [
       "Hypertext Preprocessor",
       "Hypertext Programming",
       "Hypertext Preprogramming",
-      "Hometext Preprocessor"
-    ]
+      "Hometext Preprocessor",
+    ],
   },
-    {
+  {
     numb: 4,
     question: "What does SQL stand for?",
     answer: "Structured Query Language",
@@ -41,10 +41,10 @@ let questions = [
       "Stylish Question Language",
       "Stylesheet Query Language",
       "Statement Question Language",
-      "Structured Query Language"
-    ]
+      "Structured Query Language",
+    ],
   },
-    {
+  {
     numb: 5,
     question: "What does XML stand for?",
     answer: "eXtensible Markup Language",
@@ -52,8 +52,19 @@ let questions = [
       "eXtensible Markup Language",
       "eXecutable Multiple Language",
       "eXTra Multi-Program Language",
-      "eXamine Multiple Language"
-    ]
+      "eXamine Multiple Language",
+    ],
+  },
+  {
+    numb: 6,
+    question: "What does asdf stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language",
+    ],
   },
   // you can uncomment the below codes and make duplicate as more as you want to add question
   // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
