1395db8e022f88fd03a81519cedc94072169d9a9
xxshubhamxx <shubham2002garg@gmail.com>
2021-07-14 20:20:02 +0530

    modified ds pages

--- js/ds.js
+++ js/ds.js
@@ -2,6 +2,8 @@
 // const e2=document.getElementById("usage");
 // const e3=document.getElementById("implementation");
 // const e4=document.getElementById("examples");
+// const e5=document.getElementById("references");
+// const e6=document.getElementById("courses");
 
 function clicked1(){
     document.getElementById("meaning").classList.remove("none");
@@ -10,6 +12,8 @@ function clicked1(){
     document.getElementById("usage").classList.add("none");
     document.getElementById("implementation").classList.add("none");
     document.getElementById("examples").classList.add("none");
+    document.getElementById("references").classList.add("none");
+    document.getElementById("courses").classList.add("none");
 }
 function clicked2(){
     document.getElementById("usage").classList.remove("none");
@@ -18,6 +22,8 @@ function clicked2(){
     document.getElementById("meaning").classList.add("none");
     document.getElementById("implementation").classList.add("none");
     document.getElementById("examples").classList.add("none");
+    document.getElementById("references").classList.add("none");
+    document.getElementById("courses").classList.add("none");
 }
 function clicked3(){
     document.getElementById("implementation").classList.remove("none");
@@ -26,6 +32,8 @@ function clicked3(){
     document.getElementById("meaning").classList.add("none");
     document.getElementById("usage").classList.add("none");
     document.getElementById("examples").classList.add("none");
+    document.getElementById("references").classList.add("none");
+    document.getElementById("courses").classList.add("none");
 }
 function clicked4(){
     document.getElementById("examples").classList.remove("none");
@@ -34,4 +42,26 @@ function clicked4(){
     document.getElementById("meaning").classList.add("none");
     document.getElementById("usage").classList.add("none");
     document.getElementById("implementation").classList.add("none");
+    document.getElementById("references").classList.add("none");
+    document.getElementById("courses").classList.add("none");
+}
+function clicked5(){
+    document.getElementById("references").classList.remove("none");
+    document.getElementById("rh").classList.add("animate__backInLeft");
+    document.getElementById("rd").classList.add("animate__backInRight");
+    document.getElementById("meaning").classList.add("none");
+    document.getElementById("usage").classList.add("none");
+    document.getElementById("implementation").classList.add("none");
+    document.getElementById("examples").classList.add("none");
+    document.getElementById("courses").classList.add("none");
+}
+function clicked6(){
+    document.getElementById("courses").classList.remove("none");
+    document.getElementById("ch").classList.add("animate__backInLeft");
+    document.getElementById("cd").classList.add("animate__backInRight");
+    document.getElementById("meaning").classList.add("none");
+    document.getElementById("usage").classList.add("none");
+    document.getElementById("implementation").classList.add("none");
+    document.getElementById("examples").classList.add("none");
+    document.getElementById("references").classList.add("none");
 }
