fe8643e6771018fc2beafb29632e41fb55991f23
xxshubhamxx <shubham2002garg@gmail.com>
2021-07-18 18:33:27 +0530

    modified quiz

--- js/quiz-java-questions.js
+++ js/quiz-java-questions.js
@@ -1,58 +1,58 @@
 let questions = [
-    {
-    numb: 1,
-    question: "Which of the following are not Java keywords ?",
-    answer: "then",
-    options: [
-      "double",
-      "switch",
-      "then",
-      "instanceof",
-    ]
-  },
-    {
-    numb: 2,
-    question: "Which of these have highest precedence?",
-    answer: "()",
-    options: [
-      ">>",
-      "++",
-      "()",
-      "*"
-    ]
-  },
-    {
-    numb: 3,
-    question: "Which of these is returned by operator '&' ?",
-    answer: "Hypertext Preprocessor",
-    options: [
-      "Integer",
-      "Boolean",
-      "Float",
-      "Character"
-    ]
-  },
-    {
-    numb: 4,
-    question: "Java language was initially called as ",
-    answer: "oak",
-    options: [
-      "oak",
-      "Sumatra",
-      "J++",
-      "Pine"
-    ]
-  },
-    {
-    numb: 5,
-    question: "Which one is a template for creating different objects ?",
-    answer: "eXtensible Markup Language",
-    options: [
-      "Interface",
-      "An Array",
-      "A class",
-      "Method"
-    ]
-  },
- 
+  {
+  numb: 1,
+  question: "which of the following are not Java keywords ?",
+  answer: "then",
+  options: [
+    "double",
+    "switch",
+    "then",
+    "instanceof",
+  ]
+},
+  {
+  numb: 2,
+  question: "Which of these have highest precedence?",
+  answer: "()",
+  options: [
+    ">>",
+    "++",
+    "()",
+    "*"
+  ]
+},
+  {
+  numb: 3,
+  question: "Which of these is returned by operator '&' ?",
+  answer: "Character",
+  options: [
+    "Integer",
+    "Boolean",
+    "Float",
+    "Character"
+  ]
+},
+  {
+  numb: 4,
+  question: "Java language was initially called as ",
+  answer: "oak",
+  options: [
+    "oak",
+    "Sumatra",
+    "J++",
+    "Pine"
+  ]
+},
+  {
+  numb: 5,
+  question: "Which one is a template for creating different objects ?",
+  answer: "A class",
+  options: [
+    "Interface",
+    "An Array",
+    "A class",
+    "Method"
+  ]
+},
+
 ];
