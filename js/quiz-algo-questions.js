a8d2b8e9ece52a93e06a94063004c12a84433ee3
xxshubhamxx <shubham2002garg@gmail.com>
2021-07-18 15:17:56 +0530

    added quiz files

---
+++ js/quiz-algo-questions.js
@@ -0,0 +1,58 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What is recurrence for worst case of QuickSort and what is the time complexity in Worst case?",
+    answer: "Recurrence is T(n) = T(n-1) + O(n) and time complexity is O(n^2)",
+    options: [
+      "	Recurrence is T(n) = T(n-2) + O(n) and time complexity is O(n^2).",
+      "Recurrence is T(n) = T(n-1) + O(n) and time complexity is O(n^2)",
+      "	Recurrence is T(n) = 2T(n/2) + O(n) and time complexity is O(nLogn)",
+      "Recurrence is T(n) = T(n/10) + T(9n/10) + O(n) and time complexity is O(nLogn)",
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which of the following algorithms is NOT a divide & conquer algorithm by nature?",
+    answer: "Heap Sort",
+    options: [
+      "Heap Sort",
+      "Cooley-Tukey fast Fourier transform",
+      "Euclidean algorithm to compute the greatest common divisor",
+      "Quick Sort"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Consider the polynomial p(x) = a0 + a1x + a2x^2 +a3x^3, where ai != 0, for all i. The minimum number of multiplications needed to evaluate p on an input x is:",
+    answer: "3",
+    options: [
+        "3",
+        "4",
+        "8",
+        "9"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which of the following standard algorithms is not Dynamic Programming based?",
+    answer: "Prim's Minimum Spanning Tree",
+    options: [
+      "Bellman–Ford Algorithm for single source shortest path",
+      "Floyd Warshall Algorithm for all pairs shortest paths",
+      "Prim's Minimum Spanning Tree",
+      "0-1 Knapsack problem"
+    ]
+  },
+    {
+    numb: 5,
+    question: "We use dynamic programming approach when",
+    answer: "The solution has optimal substructure",
+    options: [
+      "We need an optimal solution",
+      "The solution has optimal substructure",
+      "The given problem can be reduced to the 3-SAT problem",
+      "It's faster than Greedy"
+    ]
+  },
+ 
+];
---
+++ js/quiz-ds-questions.js
@@ -0,0 +1,58 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Which of the following points is/are true about Linked List data structure when it is compared with array?",
+    answer: "All of the above.",
+    options: [
+      "Arrays have better cache locality that can make them better in terms of performance.",
+      "It is easy to insert and delete elements in Linked List.",
+      "Random access is not allowed in a typical implementation of Linked Lists",
+      "All of the above.",
+    ]
+  },
+    {
+    numb: 2,
+    question: " Assume that reference of head of following doubly linked list is passed to above function 1 <--> 2 <--> 3 <--> 4 <--> 5 <-->6. What should be the modified linked list after the function call?",
+    answer: "6 <--> 5 <--> 4 <--> 3 <--> 2 <--> 1",
+    options: [
+      "2 <--> 1 <--> 4 <--> 3 <--> 6 <-->5",
+      "	5 <--> 4 <--> 3 <--> 2 <--> 1 <-->6",
+      "6 <--> 5 <--> 4 <--> 3 <--> 2 <--> 1",
+      "6 <--> 5 <--> 4 <--> 3 <--> 1 <--> 2"
+    ]
+  },
+    {
+    numb: 3,
+    question: " Which of the following sorting algorithms can be used to sort a random linked list with minimum time complexity?",
+    answer: "Merge Sort",
+    options: [
+        "Quick Sort",
+        "Insertion Sort",
+        "Merge Sort",
+        "Heap Sort"
+    ]
+  },
+    {
+    numb: 4,
+    question: "In the worst case, the number of comparisons needed to search a singly linked list of length n for a given element is (GATE CS 2002)",
+    answer: "n",
+    options: [
+      "n",
+      "	n/2",
+      "log 2 n – 1",
+      "	log 2 n"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Suppose each set is represented as a linked list with elements in arbitrary order. Which of the operations among union, intersection, membership, cardinality will be the slowest? (GATE CS 2004)",
+    answer: "union, intersection",
+    options: [
+      "union, intersection",
+      "membership, cardinality",
+      "intersection, membership",
+      "union only"
+    ]
+  },
+ 
+];
